2676872022104743fc69bec92ebe0f1a675ad307
Moisés Macedo <mo3ses404@gmail.com>
2022-03-16 11:07:47 -0300

    Projeto Criado

---
+++ Back/src/Eventos.API/Controllers/EventoController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Eventos.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Eventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[]{
+            new Evento(){
+                    EventoId = 1,
+                    Tema = "Angular 11 e .NET 5",
+                    Local = "Rio de Janeiro",
+                    Lote = "1º Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                    ImagemURL = "Foto.png"
+                },
+                new Evento(){
+                    EventoId = 2,
+                    Tema = "Angular e Suas Novidades",
+                    Local = "São Paulo",
+                    Lote = "2º Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                    ImagemURL = "Foto1.png"
+                }
+            };    
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+                            
+        }
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(e => e.EventoId == id);
+                            
+        }
+        [HttpPost]
+        public string Post()
+        {
+            return "ExemploPost";
+        }
+        [HttpPut("{id}")]
+        public string Put()
+        {
+            return "ExemploPut";
+        }
+        [HttpDelete("{id}")]
+        public string Delete()
+        {
+            return "ExemploDelete";
+        }
+    }
+}
