f4d2a4ff7236eb5478cb73fa29be53c8a9ba742d
Moisés Macedo <mo3ses404@gmail.com>
2022-03-16 20:13:32 -0300

    Angular Aprimorado, Interpolação, Diretiva e Binding

--- Front/EventosWeb-App/src/app/eventos/eventos.component.ts
+++ Front/EventosWeb-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  larguraImg: number = 9;
+  exibirImg: boolean = true;
+  private _filtroLista: string = '';
 
   constructor(private http: HttpClient) { }
 
@@ -18,8 +22,34 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error),
     ); //acessa a função do servidor do back
   }
+
+  toggleImagem() {
+    this.exibirImg = !this.exibirImg;
+  }
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados =  this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (e:any) => e.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      e.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1);
+  }
 }
+
+
+
